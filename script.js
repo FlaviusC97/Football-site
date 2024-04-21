2af9ae9ff1a0d6d5152498d3e0e92cbe15fcdc8b
FlaviusC97 <flavius_351@yahoo.com>
2024-04-21 15:29:52 +0300

    Html + Css Site

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.toggle("active");
+  }
+
+  function search(string){
+    window.find(string);
+    }
